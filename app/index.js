6db8ebc06b548d7917a393dfc18e1ca52ada10af
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-02-14 19:04:05 -0500

    Add a test for header generation

--- app/index.js
+++ app/index.js
@@ -1,4 +1,23 @@
-// var $ = require('jquery');
 var $ = require('jquery');
+var Clipboard = require('clipboard');
+
 require('./index.css');
-$('<div class="b-app-content"/>').html(require('./body.jade')).appendTo('body');
+
+var auth = require('../js/auth');
+
+function generate() {
+    var user = $('#b-app-user').val();
+    var apikey = $('#b-app-apikey').val();
+    var secret = $('#b-app-secretkey').val();
+    var value = 'Fill in the fields above.';
+
+    if (user && apikey && secret) {
+        value = auth(user, apikey, secret)['harbinger-authentication'];
+    }
+    $('#b-app-auth-header').val(value);
+}
+$(function () {
+    $('<div class="b-app-content"/>').html(require('./body.jade')).appendTo('body');
+    $('.b-app-generate-auth .form-group input').change(generate);
+    new Clipboard('.b-app-copy-button'); // eslint-disable-line no-new
+});
--- test/app.js
+++ test/app.js
@@ -2,7 +2,26 @@ require('../app/index');
 var $ = require('jquery');
 
 describe('Main application', function () {
+    beforeEach(function (done) {
+        function wait() {
+            if ($('.b-app-generate-auth').length > 0) {
+                done();
+            } else {
+                window.setTimeout(wait, 50);
+            }
+        }
+        wait();
+    });
     it('has navbar', function () {
         expect($('.navbar').length > 0).toBe(true);
+    });
+    it('generate auth header', function () {
+        var initial = 'Fill in the fields above.';
+        $('#b-app-user').val('user@example.com').change();
+        expect($('#b-app-auth-header').val()).toBe(initial);
+        $('#b-app-apikey').val('123456').change();
+        expect($('#b-app-auth-header').val()).toBe(initial);
+        $('#b-app-secretkey').val('654321').change();
+        expect($('#b-app-auth-header').val()).toMatch(/apikey=123456;timestamp=\d+;nonce=\d+;signature=[0-9a-f]+/);
     });
 });
